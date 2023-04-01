726680f1895e4b5fd97ae10f46c5e9f778ede6e4
Ghh99 <ghaliamkhao2022@gmail.com>
2023-04-01 12:32:00 +0300

    5

---
+++ app/src/main/java/com/example/palliativecareapppatients/Topic.java
@@ -0,0 +1,43 @@
+package com.example.palliativecareapppatients;
+
+public class Topic {
+
+    private String id;
+    private String title;
+    private String description;
+
+    public Topic() {
+        // Required empty public constructor
+    }
+
+    public Topic(String id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
