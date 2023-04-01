464079855884420a5270254a287c57aa8cb7a9d5
Ghh99 <ghaliamkhao2022@gmail.com>
2023-04-01 10:26:54 +0300

    2

--- app/src/main/java/com/example/palliativecareapppatients/MainActivity.java
+++ app/src/main/java/com/example/palliativecareapppatients/MainActivity.java
@@ -1,44 +1,56 @@
 package com.example.palliativecareapppatients;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toolbar;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
+    private MaterialToolbar toolbar;
     private ImageView searchIcon, chatIcon;
     private BottomNavigationView bottomNavigationView;
     private Fragment currentFragment;
+
+    private SharedPreferences sharedPreferences;
+
+    private static final String SHARED_PREFS = "myPrefs";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         searchIcon = findViewById(R.id.search_icon);
         chatIcon = findViewById(R.id.chat_icon);
         bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-
-
-
-
         getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new TopicList()).commit();
         currentFragment = new TopicList();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+        sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+
+        FirebaseApp.initializeApp(this, new FirebaseOptions.Builder()
+                .setApplicationId("1:965021679865:android:ac3cf346cac911d8eaa228")
+                .setApiKey("AIzaSyDulUxgYET4CY8LT8bxsVST9YAMVHWvYEQ")
+                .setDatabaseUrl("https://palliativecareapp-9e927-default-rtdb.firebaseio.com")
+                .build());
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
